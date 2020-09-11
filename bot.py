6f9adfaa28c7636f266e4b7a2669f5f3c99ab781
Artem Kropachev <artyom.kropachev@cma.ru>
2020-09-11 03:17:43 +0300

    add polom handler

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+
 import discord
 from dotenv import load_dotenv
 
@@ -19,17 +20,14 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
+    mew = [
+        'МЯУ',
+        'МЯ',
+        'МЯЯУУ'
     ]
 
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
+    if message.content == 'полом':
+        response = random.choice(mew)
         await message.channel.send(response)
 
 client.run(TOKEN)
