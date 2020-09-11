cd3aff60e11899a7949c19f2e3694af4debb8244
Artem Kropachev <artyom.kropachev@cma.ru>
2020-09-11 03:05:22 +0300

    add axample of responce to message

--- bot.py
+++ bot.py
@@ -6,20 +6,31 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD = os.getenv('DISCORD_GUILD')
+
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print(f'{client.user.name} is connected to the Discord!')
 
-#    members = '\n - '.join([member.name for member in guild.members])
-#    print(f'Guild members:\n - {members}')
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
 # %%
