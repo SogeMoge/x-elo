fd21498bf26c4092bc6eac57744f6add0750cc9e
Artem Kropachev <artyom.kropachev@cma.ru>
2020-09-15 00:07:24 +0300

    Add command for assigning role to a user

--- bot.py
+++ bot.py
@@ -15,21 +15,22 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 # 2
 bot = commands.Bot(command_prefix='%')
 
-@bot.command(name='полом', help=' - Мяукнет в ответ')
-async def mew(ctx):
-    mew_sound = ['Кошечка говорит мяу']
+# @bot.command(name='полом', help=' - Мяукнет в ответ')
+# async def mew(ctx):
+#     mew_sound = ['Кошечка говорит мяу']
 
-    response = random.choice(mew_sound)
-    await ctx.send(response)
+#     response = random.choice(mew_sound)
+#     await ctx.send(response)
 
 @bot.command(name='reg', help=' - apply league role to a user')
 @commands.has_role('league admin')
-async def addrole(ctx):
-    member = ctx.message.author
-    role = get(member.guild.roles, name="league")
-    await discord.Member.add_roles(member, role)
-
-
+async def giverole(ctx, member: discord.Member):
+    role = get(ctx.guild.roles, name="league") # role you want to add to a user
+    if role in member.roles:                   # checks if user has such role
+        await member.remove_roles(role)        #removes the role
+    else:
+        await member.add_roles(role)
+        await ctx.send(f"{member.name} has been registered in league")
 
 @bot.event
 async def on_ready():
