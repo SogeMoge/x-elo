c052902dee7ce37636bed94125100353923c5ce8
Artem Kropachev <artyom.kropachev@cma.ru>
2020-09-22 16:44:24 +0300

    k-factor changed to 32, added as var add command for leaderboard roundup for intermediate results

--- bot.py
+++ bot.py
@@ -54,25 +54,28 @@ async def status(ctx):
         embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
-@bot.command(name='game', help='submit results like @opponent and his result "@opponent win/loss"')
+@bot.command(name='list', help=' - show leaderbord', aliases=['leaderboard'])
+@commands.has_role('league')
+async def list(ctx):
+    for row in cursor.execute(f'SELECT rating,member_name FROM rating ORDER BY rating DESC'):
+        embed = discord.Embed(title="leaderboard", colour=discord.Colour(0x6790a7))
+        embed.add_field(name="Name", value=row[1], inline=True)
+        embed.add_field(name="Rating", value=row[0], inline=True)
+        await ctx.send(embed=embed)
+
+@bot.command(name='game', help='submit results like @opponent and his result "@opponent win/loss points"')
 @commands.has_role('league')
 async def results(ctx, member: discord.Member, result, points):
     pt = points
+    K = 32 # K-factor
     # extract current values for author
     for a_row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={ctx.author.id}'):
         Ra = a_row[0] # current author rating
-        # a_games = a_row[1]
-        # a_wins = a_row[2]
-        # a_losses = a_row[3]
 
     # extract current values for opponent
     for op_row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={member.id}'):
         Rop = op_row[0] # current opponent rating
-        # op_games = op_row[1]
-        # op_wins = op_row[2]
-        # op_losses = op_row[3]
  
-
     if result in 'win':
         # calculating ELO
         ## gathered delta points from current game
@@ -80,8 +83,8 @@ async def results(ctx, member: discord.Member, result, points):
         Eop = round( 1 / ( 1 + 10 ** ((Ra - Rop) / 400)), 2) # delta for opponent
 
         ## update rating
-        Rna = round( Ra + 16 * (0 - Ea), 2) # Calculate new Ra as Rna, 0 for loss
-        Rnop = round( Rop + 16 * (1 - Eop), 2) # Calculate new Rop as Rnop, 1 for win
+        Rna = round( Ra + K * (0 - Ea), 2) # Calculate new Ra as Rna, 0 for loss
+        Rnop = round( Rop + K * (1 - Eop), 2) # Calculate new Rop as Rnop, 1 for win
 
         ## Create loss game entry for author
         cursor.execute(f'INSERT INTO games (id,member_id,opponent_id,result,score) VALUES(5,{ctx.author.id},{member.id},"loss","{pt}")')
@@ -98,24 +101,26 @@ async def results(ctx, member: discord.Member, result, points):
         await ctx.send(f"{member.name} won with {points}!")
         for row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={ctx.author.id}'):
             embed = discord.Embed(title="Updated League profile", colour=discord.Colour(0x6790a7))
-            embed.add_field(name="Rating", value=row[0], inline=False)
+            embed.add_field(name="Delta", value=Ea, inline=True)
+            embed.add_field(name="Rating", value=row[0], inline=True)
             embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
             await ctx.send(embed=embed)
         for row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={member.id}'):
             embed = discord.Embed(title="Updated League profile", colour=discord.Colour(0x6790a7))
-            embed.add_field(name="Rating", value=row[0], inline=False)
+            embed.add_field(name="Delta", value=Eop, inline=True)
+            embed.add_field(name="Rating", value=row[0], inline=True)
             embed.set_footer(text=member.name, icon_url = member.avatar_url)
             await ctx.send(embed=embed)
 
     elif result in 'loss':
         # calculating ELO
         ## gathered delta points from current game
-        Ea = round( 1 / (1 + 10 ** ((Rop - Ra) / 400)) )
-        Eop = round( 1 / (1 + 10 ** ((Ra - Rop) / 400 )) )
+        Ea = round( 1 / (1 + 10 ** ((Rop - Ra) / 400)), 2)
+        Eop = round( 1 / (1 + 10 ** ((Ra - Rop) / 400 )), 2)
 
         ## update rating
-        Rna = round( Ra + 16 * (1 - Ea) ) # Calculate new Ra as Rna, 1 for win
-        Rnop = round( Rop + 16 * (0 - Eop) ) # Calculate new Rop as Rnop, 0 for loss
+        Rna = round( Ra + K * (1 - Ea), 2) # Calculate new Ra as Rna, 1 for win
+        Rnop = round( Rop + K * (0 - Eop), 2) # Calculate new Rop as Rnop, 0 for loss
 
         # Create win game entry for author
         cursor.execute(f'INSERT INTO games (id,member_id,opponent_id,result,score) VALUES(5,{ctx.author.id},{member.id},"win","{pt}")')
@@ -134,12 +139,14 @@ async def results(ctx, member: discord.Member, result, points):
         await ctx.send(f'{ctx.author.name} won with {points}!')
         for row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={ctx.author.id}'):
             embed = discord.Embed(title="Updated League profile", colour=discord.Colour(0x6790a7))
-            embed.add_field(name="Rating", value=row[0], inline=False)
+            embed.add_field(name="Delta", value=Ea, inline=True)
+            embed.add_field(name="Rating", value=row[0], inline=True)
             embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
             await ctx.send(embed=embed)
         for row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={member.id}'):
             embed = discord.Embed(title="Updated League profile", colour=discord.Colour(0x6790a7))
-            embed.add_field(name="Rating", value=row[0], inline=False)
+            embed.add_field(name="Delta", value=Eop, inline=True)
+            embed.add_field(name="Rating", value=row[0], inline=True)
             embed.set_footer(text=member.name, icon_url = member.avatar_url)
             await ctx.send(embed=embed)
     else:
