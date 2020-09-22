ab095d5bd6afc19fd33a48f65af79e1862ea0c88
Artem Kropachev <artyom.kropachev@cma.ru>
2020-09-22 19:05:41 +0300

    adding comments

--- bot.py
+++ bot.py
@@ -2,46 +2,57 @@
 import os
 import random
 import discord
+
+#load sensitive data from .env file
 from dotenv import load_dotenv
+
 from discord.ext import commands
 from discord.utils import get
 
+# load token from .env file
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
+# set prefix for bot commands
 bot = commands.Bot(command_prefix='%')
 
+# open connection to database
 import sqlite3
-conn = sqlite3.connect(os.getenv('DB')) # open database connection
+conn = sqlite3.connect(os.getenv('DB'))
 cursor = conn.cursor()
 # conn.close()  # close database connection
 
+######################
 # @bot.command(name='полом', help=' - Мяукнет в ответ')
 # async def mew(ctx):
 #     mew_sound = ['Кошечка говорит мяу']
 
 #     response = random.choice(mew_sound)
 #     await ctx.send(response)
-
+######################
 
 @bot.command(name='register', help=' - apply league role to a user', aliases=['reg'])
 @commands.has_role('league admin')
 async def giverole(ctx, member: discord.Member):
      try:
-        role = get(ctx.guild.roles, name="league") # role you want to add to a user
-        if role in member.roles:                   # checks if user has such role
+        role = get(ctx.guild.roles, name="league")                    # role you want to add to a user
+        if role in member.roles:                                      # checks if user has such role
             await ctx.send(f"{member.name} has league role already")
         else:
+            # Inserts row with user data into db as well as default game stat values
             cursor.execute(f"INSERT INTO rating (member_id, member_name) VALUES ({member.id}, '{member.name}')")
             conn.commit()
-            await member.add_roles(role) # add league role
+
+            # add league role
+            await member.add_roles(role)
+            # pretty outpun in chat
             embed = discord.Embed(title="Registration successful\nWelcome to the league!", colour=discord.Colour(0x6790a7))
             embed.set_footer(text=member.name, icon_url = member.avatar_url)
             await ctx.send(embed=embed)
-            # await ctx.send(f"{member.name} has been registered in league")
      except: # simple error handler if bot tries to insert duplicated value
          await ctx.send(f"It seems that {member.name} has rating assigned already but has no league role")
 
+# command outputs game statistics for author of the command
 @bot.command(name='status', help=' - check your personal rating', aliases=['stat'])
 @commands.has_role('league')
 async def status(ctx):
@@ -54,6 +65,7 @@ async def status(ctx):
         embed.set_footer(text=ctx.author.name, icon_url = ctx.author.avatar_url)
         await ctx.send(embed=embed)
 
+# Command shows League Leaderboard from top to bottom
 @bot.command(name='list', help=' - show leaderbord', aliases=['leaderboard'])
 @commands.has_role('league')
 async def list(ctx):
@@ -63,19 +75,24 @@ async def list(ctx):
         embed.add_field(name="Rating", value=row[0], inline=True)
         await ctx.send(embed=embed)
 
+# command for entering game results, calculaton and updating rating
 @bot.command(name='game', help='submit results like @opponent and his result "@opponent win/loss points"')
 @commands.has_role('league')
 async def results(ctx, member: discord.Member, result, points):
     pt = points
-    K = 32 # K-factor
-    # extract current values for author
+    K = 32        # K-factor
+
+    #check if players have >= 10 games with each other
+
+    # extract current rating for message author
     for a_row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={ctx.author.id}'):
         Ra = a_row[0] # current author rating
 
-    # extract current values for opponent
+    # extract current rating for mentioned opponent
     for op_row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={member.id}'):
         Rop = op_row[0] # current opponent rating
  
+    # Update rating as if mentioned opponent won the game
     if result in 'win':
         # calculating ELO
         ## gathered delta points from current game
@@ -89,16 +106,20 @@ async def results(ctx, member: discord.Member, result, points):
         ## Create loss game entry for author
         cursor.execute(f'INSERT INTO games (id,member_id,opponent_id,result,score) VALUES(5,{ctx.author.id},{member.id},"loss","{pt}")')
         conn.commit()
+        # update rating and game statistics
         cursor.execute(f'UPDATE rating SET rating = {Rna}, games = games + 1, losses = losses + 1 where member_id={ctx.author.id}')
         conn.commit()
 
         # Create win game entry for opponent
         cursor.execute(f'INSERT INTO games (id,member_id,opponent_id,result,score) VALUES(5,{member.id},{ctx.author.id},"win","{pt}")')
         conn.commit()
+        # update rating and game statistics
         cursor.execute(f'UPDATE rating SET rating = {Rnop}, games = games + 1, wins = wins + 1 where member_id={member.id}')
         conn.commit()
 
         await ctx.send(f"{member.name} won with {points}!")
+
+        # Pretty output of updated rating for participants
         for row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={ctx.author.id}'):
             embed = discord.Embed(title="Updated League profile", colour=discord.Colour(0x6790a7))
             embed.add_field(name="Delta", value=Ea, inline=True)
@@ -112,24 +133,24 @@ async def results(ctx, member: discord.Member, result, points):
             embed.set_footer(text=member.name, icon_url = member.avatar_url)
             await ctx.send(embed=embed)
 
+    # Update rating as if mentioned opponent lost the game
     elif result in 'loss':
-        # calculating ELO
-        ## gathered delta points from current game
+        ## gathered delta points from current game result
         Ea = round( 1 / (1 + 10 ** ((Rop - Ra) / 400)), 2)
         Eop = round( 1 / (1 + 10 ** ((Ra - Rop) / 400 )), 2)
 
-        ## update rating
-        Rna = round( Ra + K * (1 - Ea), 2) # Calculate new Ra as Rna, 1 for win
-        Rnop = round( Rop + K * (0 - Eop), 2) # Calculate new Rop as Rnop, 0 for loss
+        ## calculate new rating
+        Rna = round( Ra + K * (1 - Ea), 2)                    # Calculate new Ra as Rna, 1 for win
+        Rnop = round( Rop + K * (0 - Eop), 2)                 # Calculate new Rop as Rnop, 0 for loss
 
-        # Create win game entry for author
+        # Create win game entry for message author
         cursor.execute(f'INSERT INTO games (id,member_id,opponent_id,result,score) VALUES(5,{ctx.author.id},{member.id},"win","{pt}")')
         conn.commit()
         # update rating and game statistics
         cursor.execute(f'UPDATE rating SET rating = {Rna}, games = games + 1, wins = wins + 1 where member_id={ctx.author.id}')
         conn.commit()
 
-        # Create loss game entry for opponent
+        # Create loss game entry for mentioned opponent
         cursor.execute(f'INSERT INTO games (id,member_id,opponent_id,result,score) VALUES(5,{member.id},{ctx.author.id},"loss","{pt}")')
         conn.commit()
         # update rating and game statistics
@@ -137,6 +158,8 @@ async def results(ctx, member: discord.Member, result, points):
         conn.commit()
 
         await ctx.send(f'{ctx.author.name} won with {points}!')
+
+        # Pretty output of updated rating for participants 
         for row in cursor.execute(f'SELECT rating FROM rating WHERE member_id={ctx.author.id}'):
             embed = discord.Embed(title="Updated League profile", colour=discord.Colour(0x6790a7))
             embed.add_field(name="Delta", value=Ea, inline=True)
@@ -152,7 +175,7 @@ async def results(ctx, member: discord.Member, result, points):
     else:
         await ctx.send(f'Wrong result!')
 
-
+# outpud upon bot connection to the server
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
