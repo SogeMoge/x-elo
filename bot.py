7f08dd9c350e51d383a19f95f58a2d5398755501
SogeMoge <valterob@gmail.com>
2021-02-21 19:26:14 +0100

    add ladder position to status command

--- bot.py
+++ bot.py
@@ -70,8 +70,11 @@ async def giverole(ctx, member: discord.Member):
 @bot.command(name='status', help=' - check your personal rating', aliases=['stat'])
 @commands.has_role('league')
 async def status(ctx):
+    cursor.execute(f'SELECT COUNT(member_name) FROM rating WHERE rating > (SELECT rating FROM rating WHERE member_id={ctx.author.id})')
+    pos = cursor.fetchone()[0]
     for row in cursor.execute(f'SELECT rating,games,wins,losses FROM rating WHERE member_id={ctx.author.id}'):
         embed = discord.Embed(title="League profile", colour=discord.Colour(0xFFD700))
+        embed.add_field(name="Position", value=pos, inline=False)
         embed.add_field(name="Rating", value=row[0], inline=False)
         embed.add_field(name="Games", value=row[1], inline=True)
         embed.add_field(name="Wins", value=row[2], inline=True)
